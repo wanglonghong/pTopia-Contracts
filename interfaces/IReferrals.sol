46f2acc507f1b29096bb6b86788488f6f2ee546a
wanglonghong <wanglonghong4467@gmail.com>
2021-05-28 01:27:08 +0800

    create native token and libraries

---
+++ interfaces/IReferrals.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+interface IReferrals {
+
+    function updateEarn(address _member, uint256 _amount) external;
+
+    function getListReferrals(address _member) external view returns (address[] memory);
+
+    function getSponsor(address account) external view returns (address);
+
+    function isMember(address _user) external view returns (bool);
+
+    function addMember(address _member, address _parent) external;
+
+    function membersList(uint256 id) external view returns (address);
+
+}
